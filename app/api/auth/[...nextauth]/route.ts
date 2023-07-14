08f5f2a85e2d9ac1f97a8fa337415933b86fb16b
Patrick Gunnar <112112811+patrickgunnar@users.noreply.github.com>
2023-07-14 12:23:42 -0400

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,6 @@
 import NextAuth, { AuthOptions } from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import GithubProvider from 'next-auth/providers/github'
-import GoogleProvider from 'next-auth/providers/google'
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from 'bcrypt'
 import prisma from "@/app/libs/prismadb";
