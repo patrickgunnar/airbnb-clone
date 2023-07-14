3db14124f2924abc61bb519fc1227b08cf9ff773
Patrick Gunnar <112112811+patrickgunnar@users.noreply.github.com>
2023-07-14 12:06:05 -0400

    Update LoginModal.tsx

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -100,12 +100,6 @@ const LoginModal = () => {
             <hr />
             <Button
                 outline
-                label='Continue with Google'
-                icon={FcGoogle}
-                onClick={() => signIn('google')}
-            />
-            <Button
-                outline
                 label='Continue with Github'
                 icon={AiFillGithub}
                 onClick={() => signIn('github')}
